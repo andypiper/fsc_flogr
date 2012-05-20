7fe1d321e4b921c7e189ea2d771ab413ce2d5e1d
Andy Piper <andypiperuk@gmail.com>
2012-05-20 14:10:56 +0100

    Add BMP085 and tidy up

--- fsc_flogr.ino
+++ fsc_flogr.ino
@@ -6,13 +6,17 @@
  values for date, time, latitude, longitude, elevation, course, and 
  speed. Make sure the shield switch is set to DLINE.
 
- Barometric pressure added 
+ Barometric pressure from BMP085 
 */
+
+// Leaving the Serial logging enabled can cause avrdude to fail to transfer
+// compiled binary to the board!!
 
 // use TinyGPS library from arduiniana.org 
 #include <SoftwareSerial.h>
 #include <TinyGPS.h>
 #include <SD.h>
+#include <Wire.h>
 
 // pin for the SD card I/O
 const int chipSelect = 10;
@@ -21,6 +25,31 @@ const int chipSelect = 10;
 const int buttonPin = 6;     
 // variable for reading the pushbutton status
 int buttonState = LOW;         
+
+// barometer BMP085 values
+#define BMP085_ADDRESS 0x77  // I2C address of BMP085
+const unsigned char OSS = 0;  // Oversampling Setting
+
+// Calibration values
+int ac1;
+int ac2; 
+int ac3; 
+unsigned int ac4;
+unsigned int ac5;
+unsigned int ac6;
+int b1; 
+int b2;
+int mb;
+int mc;
+int md;
+
+// b5 is calculated in bmp085GetTemperature(...), 
+// this variable is also used in bmp085GetPressure(...)
+// so ...Temperature(...) must be called before ...Pressure(...).
+long b5; 
+
+short temperature;
+long pressure;
 
 // GPS TX/RX pins
 #define RXPIN 2
@@ -39,8 +68,6 @@ SoftwareSerial uart_gps(RXPIN, TXPIN);
 void getgps(TinyGPS &gps);
 char s[32];
 
-
-
 // initialize two serial ports; the standard hardware serial port 
 // (Serial()) and another serial port (NewSoftSerial()) for GPS
 void setup()
@@ -51,13 +78,16 @@ void setup()
   // might not be valid -> checksum errors
   Serial.begin(115200);
 
+  // setup BMP085
+  Wire.begin();
+  bmp085Calibration();
+
   // setup SD card or give up
   Serial.print("Initializing SD card...");
   pinMode(chipSelect, OUTPUT);
   if (!SD.begin(chipSelect)) {
     Serial.println("Card failed, or not present");
-    // don't do anything more
-    return;
+    return; // exit
   }
 
   Serial.println("card initialized.");
@@ -65,7 +95,6 @@ void setup()
   //Set baud rate of GPS
   uart_gps.begin(GPSBAUD);
 
-  Serial.println("");
   Serial.print("TinyGPS library v. "); 
   Serial.println(TinyGPS::library_version());
 
@@ -81,18 +110,15 @@ void setup()
   else {
     Serial.println("error opening datalog.txt");
   } 
-
   Serial.println("...waiting for lock...");
-  Serial.println("");
-
   // attempt to wait for GPS to settle before starting
   delay(GPSDELAY);
-
+  Serial.println("ready");
 }
 
-// This is the main loop of the code. All it does is check for data on 
+// This is the main loop of the code. Check for data on 
 // the RX pin of the Arduino, makes sure the data is valid NMEA sentences, 
-// then jumps to the getgps() function.
+// then jumps to the getdata() function.
 void loop()
 {
   // read the state of the pushbutton value:
@@ -126,27 +152,27 @@ void getdata(TinyGPS &gps)
   gps.crack_datetime(&year,&month,&day,&hour,&minute,&second,&hundredths);
 
   // Print date and time
-  Serial.print("\nDate: "); 
-  Serial.print(day, DEC); 
-  Serial.print("/"); 
-  Serial.print(month, DEC); 
-  Serial.print("/"); 
-  Serial.print(year);
-  Serial.print("  Time: "); 
-  Serial.print(hour, DEC); 
-  Serial.print(":"); 
-  Serial.print(minute, DEC); 
-  Serial.print(":"); 
-  Serial.print(second, DEC); 
-  Serial.print("."); 
-  Serial.println(hundredths, DEC);
+//  Serial.print("\nDate: "); 
+//  Serial.print(day, DEC); 
+//  Serial.print("/"); 
+//  Serial.print(month, DEC); 
+//  Serial.print("/"); 
+//  Serial.print(year);
+//  Serial.print("  Time: "); 
+//  Serial.print(hour, DEC); 
+//  Serial.print(":"); 
+//  Serial.print(minute, DEC); 
+//  Serial.print(":"); 
+//  Serial.print(second, DEC); 
+//  Serial.print("."); 
+//  Serial.println(hundredths, DEC);
+//
 
-
-  /* the following code is unreliable
+  /* FIXME: the following code is unreliable
    attempts to check for single digit date/time values and pad with zero
    should be replaced with strcat/strcpy/etc
    */
-  String mo = dtostrf(month, 2, 0, s);
+  String mo = dtostrf(month, 1, 0, s);
   if (mo.length() < 2) { 
     mo = "0" + mo; 
   }
@@ -174,25 +200,38 @@ void getdata(TinyGPS &gps)
   gps.f_get_position(&latitude, &longitude, &age);
 
   // print latitude and longitude
-  Serial.print("Lat/Long: "); 
-  Serial.print(latitude == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : latitude, 6); 
-  Serial.print(", "); 
-  Serial.println(longitude == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : longitude, 6);
+//  Serial.print("Lat/Long: "); 
+//  Serial.print(latitude == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : latitude, 6); 
+//  Serial.print(", "); 
+//  Serial.println(longitude == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : longitude, 6);
 
   // number of satellites
   int sats = gps.satellites() == TinyGPS::GPS_INVALID_SATELLITES ? 0 : gps.satellites();
-  Serial.print("Satellites in view: "); 
-  Serial.println(sats);
+//  Serial.print("Satellites in view: "); 
+//  Serial.println(sats);
 
   // print the altitude and course values
-  Serial.print("Altitude (meters): "); 
-  Serial.println(gps.f_altitude());  
-  Serial.print("Course (degrees): "); 
-  Serial.println(gps.f_course()); 
-  Serial.print("Speed (kmph): "); 
-  Serial.println(gps.f_speed_kmph());
-  Serial.println();
+//  Serial.print("Altitude (meters): "); 
+//  Serial.println(gps.f_altitude());  
+//  Serial.print("Course (degrees): "); 
+//  Serial.println(gps.f_course()); 
+//  Serial.print("Speed (kmph): "); 
+//  Serial.println(gps.f_speed_kmph());
+//  Serial.println();
 
+  temperature = bmp085GetTemperature(bmp085ReadUT());
+  pressure = bmp085GetPressure(bmp085ReadUP());
+
+//  Serial.print("Temperature: ");
+//  Serial.print(temperature, DEC);
+//  Serial.println(" *0.1 deg C");
+//  Serial.print("Pressure: ");
+//  Serial.print(pressure, DEC);
+//  Serial.println(" Pa");
+//  Serial.println();
+
+  Serial.println("data capture...");
+  
   // open and write to file
   File dataFile = SD.open("datalog.txt", FILE_WRITE);
   // if the file is available, write to it:
@@ -210,13 +249,17 @@ void getdata(TinyGPS &gps)
     dataFile.print(",");
     dataFile.print(dtostrf(longitude, 1, 5, s)); //lon
     dataFile.print(",");
-    dataFile.print(sats); //no. of sats locked
+    dataFile.print(sats); //no. of sats
     dataFile.print(",");
     dataFile.print(dtostrf(gps.f_altitude(),1,2,s)); //alt
     dataFile.print(",");
     dataFile.print(dtostrf(gps.f_course(),1,2,s)); //course
     dataFile.print(",");
-    dataFile.println(dtostrf(gps.f_speed_kmph(),1,2,s)); // speed and last line
+    dataFile.print(dtostrf(gps.f_speed_kmph(),1,2,s)); //speed kmph
+    dataFile.print(",");
+    dataFile.print(dtostrf(temperature,1,2,s)); //temp *0.1 deg C
+    dataFile.print(",");
+    dataFile.println(dtostrf(pressure,1,2,s)); //pressure Pa
     dataFile.close();
   }  
   // if the file isn't open, write an error to serial
@@ -225,4 +268,164 @@ void getdata(TinyGPS &gps)
   } 
 
 }
+
+/* helper functions for the barometer BMP085 */
+
+// Stores all of the bmp085's calibration values into global variables
+// Calibration values are required to calculate temp and pressure
+// This function should be called at the beginning of the program
+void bmp085Calibration()
+{
+  ac1 = bmp085ReadInt(0xAA);
+  ac2 = bmp085ReadInt(0xAC);
+  ac3 = bmp085ReadInt(0xAE);
+  ac4 = bmp085ReadInt(0xB0);
+  ac5 = bmp085ReadInt(0xB2);
+  ac6 = bmp085ReadInt(0xB4);
+  b1 = bmp085ReadInt(0xB6);
+  b2 = bmp085ReadInt(0xB8);
+  mb = bmp085ReadInt(0xBA);
+  mc = bmp085ReadInt(0xBC);
+  md = bmp085ReadInt(0xBE);
+}
+
+// Calculate temperature given ut.
+// Value returned will be in units of 0.1 deg C
+short bmp085GetTemperature(unsigned int ut)
+{
+  long x1, x2;
+  
+  x1 = (((long)ut - (long)ac6)*(long)ac5) >> 15;
+  x2 = ((long)mc << 11)/(x1 + md);
+  b5 = x1 + x2;
+
+  return ((b5 + 8)>>4);  
+}
+
+// Calculate pressure given up
+// calibration values must be known
+// b5 is also required so bmp085GetTemperature(...) must be called first.
+// Value returned will be pressure in units of Pa.
+long bmp085GetPressure(unsigned long up)
+{
+  long x1, x2, x3, b3, b6, p;
+  unsigned long b4, b7;
+  
+  b6 = b5 - 4000;
+  // Calculate B3
+  x1 = (b2 * (b6 * b6)>>12)>>11;
+  x2 = (ac2 * b6)>>11;
+  x3 = x1 + x2;
+  b3 = (((((long)ac1)*4 + x3)<<OSS) + 2)>>2;
+  
+  // Calculate B4
+  x1 = (ac3 * b6)>>13;
+  x2 = (b1 * ((b6 * b6)>>12))>>16;
+  x3 = ((x1 + x2) + 2)>>2;
+  b4 = (ac4 * (unsigned long)(x3 + 32768))>>15;
+  
+  b7 = ((unsigned long)(up - b3) * (50000>>OSS));
+  if (b7 < 0x80000000)
+    p = (b7<<1)/b4;
+  else
+    p = (b7/b4)<<1;
+    
+  x1 = (p>>8) * (p>>8);
+  x1 = (x1 * 3038)>>16;
+  x2 = (-7357 * p)>>16;
+  p += (x1 + x2 + 3791)>>4;
+  
+  return p;
+}
+
+// Read 1 byte from the BMP085 at 'address'
+char bmp085Read(unsigned char address)
+{
+  unsigned char data;
+  
+  Wire.beginTransmission(BMP085_ADDRESS);
+  Wire.write(address);
+  Wire.endTransmission();
+  
+  Wire.requestFrom(BMP085_ADDRESS, 1);
+  while(!Wire.available())
+    ;
+    
+  return Wire.read();
+}
+
+// Read 2 bytes from the BMP085
+// First byte will be from 'address'
+// Second byte will be from 'address'+1
+int bmp085ReadInt(unsigned char address)
+{
+  unsigned char msb, lsb;
+  
+  Wire.beginTransmission(BMP085_ADDRESS);
+  Wire.write(address);
+  Wire.endTransmission();
+  
+  Wire.requestFrom(BMP085_ADDRESS, 2);
+  while(Wire.available()<2)
+    ;
+  msb = Wire.read();
+  lsb = Wire.read();
+  
+  return (int) msb<<8 | lsb;
+}
+
+// Read the uncompensated temperature value
+unsigned int bmp085ReadUT()
+{
+  unsigned int ut;
+  
+  // Write 0x2E into Register 0xF4
+  // This requests a temperature reading
+  Wire.beginTransmission(BMP085_ADDRESS);
+  Wire.write(0xF4);
+  Wire.write(0x2E);
+  Wire.endTransmission();
+  
+  // Wait at least 4.5ms
+  delay(5);
+  
+  // Read two bytes from registers 0xF6 and 0xF7
+  ut = bmp085ReadInt(0xF6);
+  return ut;
+}
+
+// Read the uncompensated pressure value
+unsigned long bmp085ReadUP()
+{
+  unsigned char msb, lsb, xlsb;
+  unsigned long up = 0;
+  
+  // Write 0x34+(OSS<<6) into register 0xF4
+  // Request a pressure reading w/ oversampling setting
+  Wire.beginTransmission(BMP085_ADDRESS);
+  Wire.write(0xF4);
+  Wire.write(0x34 + (OSS<<6));
+  Wire.endTransmission();
+  
+  // Wait for conversion, delay time dependent on OSS
+  delay(2 + (3<<OSS));
+  
+  // Read register 0xF6 (MSB), 0xF7 (LSB), and 0xF8 (XLSB)
+  Wire.beginTransmission(BMP085_ADDRESS);
+  Wire.write(0xF6);
+  Wire.endTransmission();
+  Wire.requestFrom(BMP085_ADDRESS, 3);
+  
+  // Wait for data to become available
+  while(Wire.available() < 3)
+    ;
+  msb = Wire.read();
+  lsb = Wire.read();
+  xlsb = Wire.read();
+  
+  up = (((unsigned long) msb << 16) | ((unsigned long) lsb << 8) | (unsigned long) xlsb) >> (8-OSS);
+  
+  return up;
+}
+
 
